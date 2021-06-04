8c09ccc36e55dd79a155295d86766f3c9577153d
Jieun Kang <jieunkang101@gmail.com>
2021-06-04 12:29:14 -0700

    fix styling issue

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -13,17 +13,15 @@ const Editor = ({
 }: EditorProps) => (
   <section className={styles.editor}>
     <h1 className={styles.title}>Card Maker</h1>
-    <ul>
-      {Object.keys(cards).map((key) => (
-        <CardEditForm
-          key={key}
-          FileInput={FileInput}
-          card={cards[key]}
-          updateCard={updateCard}
-          deleteCard={deleteCard}
-        />
-      ))}
-    </ul>
+    {Object.keys(cards).map((key) => (
+      <CardEditForm
+        key={key}
+        FileInput={FileInput}
+        card={cards[key]}
+        updateCard={updateCard}
+        deleteCard={deleteCard}
+      />
+    ))}
     <CardAddForm FileInput={FileInput} onAdd={addCard} />
   </section>
 );
--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -6,7 +6,7 @@ import { PreviewProps } from '../../types/Camping';
 const Preview = ({ cards }: PreviewProps) => (
   <section className={styles.preview}>
     <h1 className={styles.title}>Card Preview</h1>
-    <ul>
+    <ul className={styles.cards}>
       {Object.keys(cards).map((key) => (
         <Card key={key} card={cards[key]} />
       ))}
